7c97be44bc51a5f7ce1b7c1d9dc90172da6c6a6c
Philipp Keese <philipp.keese@student.hpi.de>
2022-01-15 18:30:15 +0100

    move camera

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,16 +1,12 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+export var max_angle := 20.0
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	var center := Vector3.ZERO
+	for player in $Players.get_children():
+		center += player.global_transform.origin * Vector3(1.0, 0.0, 1.0)
+	center /= $Players.get_child_count()
+	$MeshInstance.global_transform.origin = center + Vector3.UP * 3.0
