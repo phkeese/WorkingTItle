13450a4ab94ae5c38491a292d880e2863f1bd9ea
Philipp Keese <philipp.keese@student.hpi.de>
2022-01-15 18:50:08 +0100

    .

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 
 export var max_angle := 20.0
-export var angle := 0.0
+export var angle := 0.0 setget _set_angle
 export var player_weight := 1.0
 export var max_distance := 6.0
 
@@ -16,6 +16,11 @@ func _process(delta: float) -> void:
 	
 	var tip = clamp(center.z, -max_distance, max_distance) / max_distance
 	var angle_delta : float = player_weight * tip * delta
-	angle = clamp(angle + angle_delta, -max_angle, max_angle)
+	self.angle += angle_delta
 	
+	$CameraContainer.rotation_degrees = Vector3(-angle, 0.0, 0.0)
+
+
+func _set_angle(new_angle: float) -> void:
+	angle = clamp(new_angle, -max_angle, max_angle)
 	$CameraContainer.rotation_degrees = Vector3(-angle, 0.0, 0.0)
