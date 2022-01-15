e43b740348d0ae161f5ec32101ed5d19eefb8df0
Philipp Keese <philipp.keese@student.hpi.de>
2022-01-15 14:25:53 +0100

    refactor station

--- scripts/station.gd
+++ scripts/station.gd
@@ -1,9 +1,22 @@
-extends Area
+extends Spatial
 
 
-func _on_Station_body_entered(body: Node) -> void:
-	print("enter %s" % body)
+export var title := "Station" setget _set_title
+export(float, 0.0, 1.0) var health := 1.0 setget _set_health
 
 
-func _on_Station_body_exited(body: Node) -> void:
-	print("exit %s" % body)
+func damage(amount: float) -> void:
+	self.health -= amount
+
+
+func heal(amount: float) -> void:
+	self.health += amount
+
+
+func _set_title(new_title: String) -> void:
+	$HUD/VBoxContainer/Label.text = new_title
+
+
+func _set_health(new_health: float) -> void:
+	health = clamp(new_health, 0.0, 1.0)
+	$HUD/VBoxContainer/ProgressBar.value = health
