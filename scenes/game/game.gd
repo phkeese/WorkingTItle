60a736ea6425d80e77edb749276f5d2e72349cef
Philipp Keese <philipp.keese@student.hpi.de>
2022-01-15 18:43:47 +0100

    lean ship

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -2,6 +2,9 @@ extends Spatial
 
 
 export var max_angle := 20.0
+export var angle := 0.0
+export var player_weight := 1.0
+export var max_distance := 6.0
 
 
 func _process(delta: float) -> void:
@@ -10,3 +13,9 @@ func _process(delta: float) -> void:
 		center += player.global_transform.origin * Vector3(1.0, 0.0, 1.0)
 	center /= $Players.get_child_count()
 	$MeshInstance.global_transform.origin = center + Vector3.UP * 3.0
+	
+	var tip = clamp(center.z, -max_distance, max_distance) / max_distance
+	var angle_delta : float = player_weight * tip * delta
+	angle = clamp(angle + angle_delta, -max_angle, max_angle)
+	
+	$CameraContainer.rotation_degrees = Vector3(-angle, 0.0, 0.0)
