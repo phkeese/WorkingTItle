9bb1d54815d9228fbf4fd8bd5f0a0e41f5126b98
Philipp Keese <philipp.keese@student.hpi.de>
2022-01-15 12:06:52 +0100

    move player

--- player/player.gd
+++ player/player.gd
@@ -1,14 +1,27 @@
-extends Node2D
+extends Spatial
 
 var color setget set_color
+export var speed := 1.0
+
 
 func _network_ready(is_source):
 	if is_source:
 		set_color(Color.from_hsv(rand_range(1, 360), 1, 1))
-		position = Vector2(rand_range(0, 600), rand_range(0, 400))
+		translation = Vector3(rand_range(-1,1), 0, rand_range(-1,1))
 	# same value on all clients now!
 	print(color)
 
+
+func _process(delta: float) -> void:
+	var right := Input.get_action_strength("player_1_right") - Input.get_action_strength("player_1_left")
+	var up := Input.get_action_strength("player_1_up") - Input.get_action_strength("player_1_down")
+	var movement := Vector3.RIGHT * right + Vector3.FORWARD * up
+	translation += movement * speed * delta
+	
+
+
 func set_color(c: Color):
 	color = c
-	$token.color = c
+	var mat := SpatialMaterial.new()
+	mat.albedo_color = color
+	$MeshInstance.material_override = mat
