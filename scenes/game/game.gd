3d402811b23a1d298b66903d9207f7d18fbce366
Philipp Keese <philipp.keese@student.hpi.de>
2022-01-15 20:52:24 +0100

    .

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -5,6 +5,7 @@ export var max_angle := 20.0
 export var angle := 0.0 setget _set_angle
 export var player_weight := 1.0
 export var max_distance := 6.0
+export var tilt_curve : Curve
 
 
 func _process(delta: float) -> void:
@@ -15,6 +16,7 @@ func _process(delta: float) -> void:
 	$MeshInstance.global_transform.origin = center + Vector3.UP * 3.0
 	
 	var tip = clamp(center.z, -max_distance, max_distance) / max_distance
+	tip = sign(tip) * tilt_curve.interpolate_baked(abs(tip))
 	var angle_delta : float = player_weight * tip * delta
 	self.angle += angle_delta
 
@@ -22,4 +24,4 @@ func _process(delta: float) -> void:
 func _set_angle(new_angle: float) -> void:
 	angle = clamp(new_angle, -max_angle, max_angle)
 	$CameraContainer.rotation_degrees = Vector3(-angle, 0.0, 0.0)
-	$CameraContainer/Camera.rotation_degrees.z = angle
+	$CameraContainer/Camera.rotation_degrees.z = angle / 5.0
