fa168502a0946b84acffab2cc018e33a1a77a9f4
Philipp Keese <philipp.keese@student.hpi.de>
2022-01-16 12:54:55 +0100

    so many assets

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -6,6 +6,8 @@ export var angle := 0.0 setget _set_angle
 export var player_weight := 1.0
 export var max_distance := 6.0
 export var tilt_curve : Curve
+export var max_pitch := 20.0
+export var height := 100.0 setget _set_height
 
 
 func _process(delta: float) -> void:
@@ -19,9 +21,19 @@ func _process(delta: float) -> void:
 	tip = sign(tip) * tilt_curve.interpolate_baked(abs(tip))
 	var angle_delta : float = player_weight * tip * delta
 	self.angle += angle_delta
+	
+	var speed := $Ship.speed() as float
+	$CameraContainer.rotation_degrees.z = max_pitch * (1.0 - speed)
+	
+	self.height -= 1.0 - speed
 
 
 func _set_angle(new_angle: float) -> void:
 	angle = clamp(new_angle, -max_angle, max_angle)
 	$CameraContainer.rotation_degrees = Vector3(-angle, 0.0, 0.0)
 	$CameraContainer/Camera.rotation_degrees.z = angle / 5.0
+
+
+func _set_height(new_height: float) -> void:
+	height = new_height
+	$HUD/PanelContainer/VBoxContainer/Height.value = new_height
--- scripts/ship.gd
+++ scripts/ship.gd
@@ -4,10 +4,23 @@ extends Spatial
 onready var _rotor := $Ship/propeller
 
 
+func _ready() -> void:
+	var lwing := $"Ship/wing_l_flapping-1hz/AnimationPlayer" as AnimationPlayer
+	lwing.get_animation("ArmatureAction").loop = true
+	$"Ship/wing_l_flapping-1hz/AnimationPlayer".play("ArmatureAction")
+
+
 func _on_give(to: Player, item_scene: PackedScene) -> void:
 	var item := item_scene.instance()
 	to.item = item
 
 
 func _process(delta: float) -> void:
-	$Ship/propeller/AnimationPlayer.playback_speed = $Stations/Engine.speed()
+	var speed :=  $Stations/Engine.speed() as float
+	$Ship/propeller/AnimationPlayer.playback_speed = speed
+	$"Ship/wing_l_flapping-1hz/AnimationPlayer".playback_speed = speed / 5.0
+	$"Ship/wing_l_flapping-1hz2/AnimationPlayer".playback_speed = speed / 5.0
+	
+
+func speed() -> float:
+	return $Stations/Engine.speed() as float
